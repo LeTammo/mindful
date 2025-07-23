4e2daf43611da7ce57d775758341bb9dc42d9943
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2025-07-23 12:11:25 +0200

    Fix errors

--- src/app/components/types.ts
+++ src/app/components/types.ts
@@ -2,10 +2,9 @@ export type OutputItem =
   | { type: 'command'; query: string }
   | { type: 'feedback'; message: string }
   | { type: 'file-match'; path: string; line?: string; lineNumber?: number }
-  | { type: 'content'; content: string }
+  | { type: 'content'; content: string; path: string }
   | { type: 'edit-button'; path: string; content: string };
 
 export type Suggestion =
   | { type: 'feedback'; message: string }
   | { type: 'file-match'; path: string; line?: string };
-
